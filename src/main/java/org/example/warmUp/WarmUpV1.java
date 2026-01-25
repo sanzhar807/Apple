ea0dd00065d019d2b4ea99012cd2cab00444d4da
sanzhar807 <amanturov2471@gmail.com>
2026-01-25 19:04:11 +0700

    First 12 tasks of WarmUp 1
    * Third sumDouble
    
    * Forth diff21
    
    * Fifth parrotTrouble
    
    * Sixth makes10
    
    * Seven nearHundred
    
    * Eath posNeg
    
    * Ninth notString
    
    * Ten missingChar
    
    * eleventh frontBack
    
    * twelvth front3

--- src/main/java/org/example/warmUp/WarmUpV1.java
+++ src/main/java/org/example/warmUp/WarmUpV1.java
@@ -8,12 +8,62 @@ public class WarmUpV1 {
         }
         return false;
     }
-
     public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
         if(aSmile && bSmile) return true;
         if(!aSmile && !bSmile) return true;
         if (aSmile || bSmile ) return false;
         return false;
     }
+    public int sumDouble(int a, int b) {
+        int sum=a+b;
+        if(a==b){
+            sum=sum*2;
+        }
 
+        return sum;
+    }
+    public int diff21(int n) {
+        if(n<= 21){
+            return 21-n;
+        }else{
+            return(n-21)*2;
+        }
+    }
+    public boolean parrotTrouble(boolean talking, int hour) {
+        return(talking && (hour<7 || hour>20));
+    }
+    public boolean makes10(int a, int b) {
+        if(a==10 || b==10) return true;
+        else if (a+b == 10) return true;
+        return false;
+    }
+    public boolean nearHundred(int n) {
+        if(n >=90 && n<=110) return true;
+        if(n>= 190 && n<= 210) return true;
+        return false;
+    }
+    public boolean posNeg(int a, int b, boolean negative) {
+        if ( negative == true && a<0 && b <0) return true;
+        if(a<0 && b>0 && negative !=true) return true;
+        if(a>0 && b<0 && negative != true) return true;
+        return false;
+    }
+    public String notString(String str) {
+        if(str.startsWith("not")) return str;
+        return "not ".concat(str);
+    }
+    public String missingChar(String str, int n) {
+        StringBuilder word = new StringBuilder(str);
+        word.deleteCharAt(n);
+        return word.toString();
+    }
+    public String frontBack(String str) {
+        if(str.length() <= 1)return str;
+        return str.substring(str.length()-1).concat(str.substring(1,str.length()-1)).concat(str.substring(0,1));
+    }
+    public String front3(String str) {
+        if (str.length() <1) return str;
+        if(str.length()<=3) return str.concat(str).concat(str);
+        return str.substring(0,3).concat(str.substring(0,3)).concat(str.substring(0,3));
+    }
 }
--- src/test/java/warmUp/WarmUpV1Test.java
+++ src/test/java/warmUp/WarmUpV1Test.java
@@ -36,4 +36,74 @@ public class WarmUpV1Test {
         Assertions.assertTrue(obj.monkeyTrouble(false,false));
         Assertions.assertFalse(obj.monkeyTrouble(false,true));
     }
+
+    @BeforeEach
+    void beforeEach1(){
+        System.out.println("Third");
+    }
+    @Test
+    void sumDoubleT(){
+        Assertions.assertEquals(3,obj.sumDouble(1,2));
+        Assertions.assertEquals(8,obj.sumDouble(2,2));
+        Assertions.assertEquals(12,obj.sumDouble(3,3));
+    }
+
+    @Test
+    void diff21T(){
+        Assertions.assertEquals(2,obj.diff21(19));
+        Assertions.assertEquals(10,obj.diff21(11));
+        Assertions.assertEquals(21,obj.diff21(0));
+    }
+
+    @Test
+    void parrotTroubleT(){
+        Assertions.assertTrue(obj.parrotTrouble(true,6));
+        Assertions.assertFalse(obj.parrotTrouble(true,7));
+        Assertions.assertTrue(obj.parrotTrouble(true,21));
+    }
+
+    @Test
+    void makes10T(){
+        Assertions.assertTrue(obj.makes10(9,10));
+        Assertions.assertFalse(obj.makes10(9,9));
+        Assertions.assertTrue(obj.makes10(1,9));
+    }
+
+    @Test
+    void nearHundredT(){
+        Assertions.assertTrue(obj.nearHundred(93));
+        Assertions.assertTrue(obj.nearHundred(90));
+        Assertions.assertFalse(obj.nearHundred(89));
+    }
+
+    @Test
+    void posNegT(){
+        Assertions.assertTrue(obj.posNeg(1,-1,false));
+        Assertions.assertFalse(obj.posNeg(-4,-5,false));
+        Assertions.assertTrue(obj.posNeg(5,-5,false));
+    }
+    @Test
+    void notStringT(){
+        Assertions.assertEquals("not candy",obj.notString("not candy"));
+        Assertions.assertEquals("not bad",obj.notString("not bad"));
+        Assertions.assertEquals("not x",obj.notString("x"));
+    }
+    @Test
+    void missingCharT(){
+        Assertions.assertEquals("ktten",obj.missingChar("kitten",1));
+        Assertions.assertEquals("itten",obj.missingChar("kitten",0));
+        Assertions.assertEquals("i",obj.missingChar("Hi",0));
+    }
+    @Test
+    void frontBackT(){
+        Assertions.assertEquals("eodc",obj.frontBack("code"));
+        Assertions.assertEquals("ehocolatC",obj.frontBack("Chocolate"));
+        Assertions.assertEquals("aavJ",obj.frontBack("Java"));
+    }
+    @Test
+    void front3T(){
+        Assertions.assertEquals("JavJavJav",obj.front3("Java"));
+        Assertions.assertEquals("ChoChoCho",obj.front3("Chocolate"));
+        Assertions.assertEquals("aaa",obj.front3("a"));
+    }
 }
