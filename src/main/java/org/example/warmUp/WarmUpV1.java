265fb2c73bb9822915ab9708ffb5c01b90250ed6
sanzhar807 <amanturov2471@gmail.com>
2026-01-25 17:53:37 +0700

    First sleepIn

---
+++ src/main/java/org/example/warmUp/WarmUpV1.java
@@ -0,0 +1,13 @@
+package org.example.warmUp;
+
+public class WarmUpV1 {
+
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if (!weekday || vacation){
+            return true;
+        }
+        return false;
+    }
+
+
+}
---
+++ src/test/java/warmUp/WarmUpV1Test.java
@@ -0,0 +1,29 @@
+package warmUp;
+
+import org.example.warmUp.WarmUpV1;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class WarmUpV1Test {
+    WarmUpV1 obj = new WarmUpV1();
+
+    @BeforeAll
+    static void beforAll(){
+        System.out.println("Начало WarmUp 1");
+    }
+
+    @BeforeEach
+    void beforEach(){
+        System.out.println("First");
+    }
+    @Test
+    void sleepInT() {
+        Assertions.assertTrue(obj.sleepIn(false,false));
+        Assertions.assertFalse(obj.sleepIn(true,false));
+        Assertions.assertTrue(obj.sleepIn(false,true));
+    }
+
+
+}
