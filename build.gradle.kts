6db16d7241fde29d4a23a9eb3d5ad23bf262ea7c
sanzhar807 <amanturov2471@gmail.com>
2026-01-26 10:59:31 +0700

    Koko

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,10 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.junit.jupiter:junit-jupiter-api:6.0.2")
     testImplementation("org.junit.platform:junit-platform-launcher:6.0.2")
+// Source: https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-java
+    implementation("org.seleniumhq.selenium:selenium-java:4.40.0")
+// Source: https://mvnrepository.com/artifact/io.github.bonigarcia/webdrivermanager
+    implementation("io.github.bonigarcia:webdrivermanager:6.3.3")
 }
 
 tasks.test {
---
+++ src/test/java/junit/JunitDemo.java
@@ -0,0 +1,28 @@
+package junit;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class JunitDemo {
+    @Test
+    @Tag("Smoke")
+    @DisplayName("Проверка регистрации нового пользователя")
+    void registrationTest1(){
+        System.out.println("Регистрация нового пользователя 1");
+    }
+    @Test
+    @DisplayName("Проверка регистрации нового пользователя")
+   @Disabled("Ведется исправление фичи")
+    @Tag("Regression")
+    void registrationTest2(){
+        System.out.println("Регистрация нового пользователя 2");
+    }
+    @Test
+    @DisplayName("Проверка регистрации нового пользователя")
+   @Tag("Payment")
+    void registrationTest3(){
+        System.out.println("Регистрация нового пользователя 3");
+    }
+}
