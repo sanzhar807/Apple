87f74c49f844ba831cb2d5f231372c6bea75465d
sanzhar807 <amanturov2471@gmail.com>
2026-04-02 12:23:50 +0700

    Add headless

--- src/main/java/org/example/demoQa/drivers/ChromeWebDriver.java
+++ src/main/java/org/example/demoQa/drivers/ChromeWebDriver.java
@@ -3,13 +3,16 @@ package org.example.demoQa.drivers;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class ChromeWebDriver {
     public static WebDriver driver;
 
     public static WebDriver loadChromeDriver(){
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         return driver;
     }
